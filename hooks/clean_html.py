9cef8a9db16df1e42147d100fa1e555c0500c962
Darvis <dsouzavishal685@gmail.com>
2026-04-26 20:12:01 +0530

    Revert "New site"
    This reverts commit 9fe1326333d7caba43c9998adb1edf4f72e08303.

--- hooks/clean_html.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-def on_post_page(output, **kwargs):
-    # This parses the HTML and re-indents everything perfectly.
-    # Not really needed but I like to see well indented html
-    soup = BeautifulSoup(output, 'html.parser')
-    return soup.prettify()
